15916470a5df11c79600daeafe2685f7b4fa3188
AJAY R <63496620+ajayravi238@users.noreply.github.com>
2023-06-14 20:38:09 +0530

    Add files via upload

---
+++ Muson - Music Player/emotion-based-music-player-master/WD_INNOVATIVE/code.js
@@ -0,0 +1,211 @@
+var songrun=false;
+var count=1;
+var mod=1;
+var path=["songs\\Mavane.mp3",
+"songs\\Kalippu.mp3",
+"songs\\Karuppu-Vellai.mp3",
+"songs\\Sirikkalam-Parakkalam.mp3",
+"songs\\Saroja-Saman-Nikalo.mp3",
+"songs\\Vaadi-Pulla-Vaadi.mp3",
+"songs\\Venmathiye.mp3",
+"songs\\Ava-Enna-Enna.mp3",
+"songs\\Kadhal-Sadugudu.mp3",
+"songs\\Journey.mp3"];
+
+var sname=["Mavane",
+"Kalippu",
+"Karuppu Vellai",
+"Sirikkalam Parakkalam",
+"Saroja Saman Nikolo",
+"Vaadi Pulla Vaadi",
+"Venmathiye",
+"Ava Enna",
+"Kadhal Sadugudu",
+"Journey"];
+
+var sd=["Artist: Arivu and Vivek–Mervin<br>Movie: Pattas<br>Released: 2019",
+"Artists: Murali Gopy, Shabareesh Varma<br>Movie: Premam<br>Released: 2015",
+"Artists: Sam C. S. and Sivam<br>Movie: Vikram Vedha<br>Released: 2017",
+"Artist: Benny Dayal , Madurai Soulijour<br>Movie: Kannum Kannum Kollaiyadithaal<br>Released: 2020",
+"Artist: Shankar Mahadevan ,Premgi Amaren<br>Movie: Chennai 600028<br>Released: 2007",
+"Artists: Hiphop Tamizha<br>Movie: Meesaya Murukku<br>Released: 2017",
+"Artists: Roop Kumar Rathod and Tippu<br>Movie: Minnale<br>Released: 2001",
+"Artist: Harris Jayaraj, Karthik, and Prassanna<br>Movie: Vaaranam Aayiram<br>Released: 2008",
+"Artist: S.P. Charan<br>Movie: Alaipayuthey<br>Released: 2000",
+"Artists: Pradeep Kumar<br>Movie: Jaanu<br>Released: 2020"];
+
+var bool=[];
+for(var i=0; i<sd.length; i++)
+	bool[i]=false;
+
+var icon=["images\\\\1.jpg",
+"images\\\\2.jpg",
+"images\\\\3.jpg",
+"images\\\\4.jpg",
+"images\\\\5.jpg",
+"images\\\\6.jpg",
+"images\\\\7.jpg",
+"images\\\\8.jpg",
+"images\\\\9.jpg",
+"images\\\\10.jpg"];
+
+var mood=[["1","2","3"],["4","5"],["6","7","8"],["9","10"]];
+var mmm=["1.png","1.png","1.png","2.png","2.png","3.png","3.png","3.png","4.png","4.png"];
+
+var songs=new Array(icon.length);
+for (var i = 0; i<icon.length; i++) {
+	songs[i]=new Array(4);
+	songs[i][0]=path[i];
+	songs[i][1]=sd[i];
+	songs[i][2]=icon[i];
+	songs[i][3]=mmm[i];
+	console.log(songs[i][0]);
+	console.log(songs[i][1]);
+	console.log(songs[i][2]);
+	var ins=document.createElement("div");
+	ins.id='b'+i;
+	//ins.onclick=function(){
+	//next(this);
+  	//};
+	ins.setAttribute("class", "song");
+	document.body.appendChild(ins);
+	document.getElementById('b'+i).innerHTML='<div id="pic" style=\'background-image: url(\"'+songs[i][2]+'\");\'>  <input type="button" id="'+"a"+i+'" class="play" > <input type="button" id="'+"c"+i+'" class="add">  </div><div id="data"><br><br>'+songs[i][1]+'</div>';
+	document.getElementById('a'+i).onclick=function(){
+		play(this);
+	};
+	document.getElementById('c'+i).onclick=function(){
+		addq(this);
+	};	
+}
+
+
+
+
+function setmod(elem){
+	mod=elem.value;
+	if(!songrun){
+		if(mod==2)
+			getTime();
+		if(mod==3)
+			rand_play();
+	}
+}
+
+function play(elem){
+	console.log(elem.id);
+	var x=elem.id.charAt(1);
+	var z=songs[x][0];
+	document.getElementById("sname").innerHTML=sname[x];
+	document.getElementById("sel").src= z;
+	document.getElementById("main_slider").load();
+	document.getElementById("main_slider").play();
+	document.getElementById("emoji").style.backgroundImage="url('"+songs[x][3]+"')";
+	songrun=true;
+
+}
+
+var eqc=1;
+var sqc=1;
+
+function addq(elem){
+	console.log(elem.id);
+	var x=elem.id.charAt(1);
+	if(!songrun){
+		var z=songs[x][0];
+		document.getElementById("sname").innerHTML=sname[x];
+		document.getElementById("sel").src= z;
+		document.getElementById("main_slider").load();
+		document.getElementById("main_slider").play();
+		document.getElementById("emoji").style.backgroundImage="url('"+songs[x][3]+"')";
+		songrun=true;		
+		return;
+	}
+	if(bool[x]==true)
+		return;
+	
+	bool[x]=true;
+	var l=document.createElement("label");
+	l.id="e"+eqc;
+	l.name=x;
+	l.innerHTML=sname[x]+"<br>";
+	//var text=document.createTextNode(sname[x]+"<br>");
+	//l.appendChild(text);
+	document.getElementById("queue").appendChild(l);
+	eqc=eqc+1;
+}
+
+function nextsong(){
+	if(sqc==eqc){
+				alert("Queue is empty.");
+				return;
+		}
+		var elem=document.getElementById("e"+sqc);
+			var xa=elem.name;
+			var pa=songs[xa][0];
+			bool[xa]=false;
+			document.getElementById("sname").innerHTML=sname[xa];
+			document.getElementById("sel").src= pa;
+			document.getElementById("main_slider").load();
+			document.getElementById("main_slider").play();
+			document.getElementById("emoji").style.backgroundImage="url('"+songs[xa][3]+"')";
+			
+			songrun=true;
+			document.getElementById("queue").removeChild(elem);	
+			sqc=sqc+1;
+
+}
+
+function next_in_Q(){
+			songrun=false;
+			if(sqc==eqc){
+				alert("Queue is empty.");
+				return;
+			}
+			var elem=document.getElementById("e"+sqc);
+			var xa=elem.name;
+			var pa=songs[xa][0];
+			document.getElementById("sname").innerHTML=sname[xa];
+			document.getElementById("sel").src= pa;
+			document.getElementById("main_slider").load();
+			document.getElementById("main_slider").play();
+			document.getElementById("emoji").style.backgroundImage="url('"+songs[xa][3]+"')";
+			songrun=true;
+			document.getElementById("queue").removeChild(elem);	
+			sqc=sqc+1;
+			}
+
+function rand_play(){
+	var index=Math.random()*path.length;
+	index=parseInt(index);
+	var pa=songs[index][0];
+	document.getElementById("sname").innerHTML=sname[index];
+	document.getElementById("sel").src= pa;
+	document.getElementById("main_slider").load();
+	document.getElementById("main_slider").play();
+	document.getElementById("emoji").style.backgroundImage="url('"+songs[index][3]+"')";
+	songrun=true;
+
+}
+function moody(val){
+	var index=Math.random()*mood[val].length;
+	index=parseInt(index);
+	var pa=songs[mood[val][index]-1][0];
+	document.getElementById("sname").innerHTML=sname[mood[val][index]-1];
+	document.getElementById("sel").src= pa;
+	document.getElementById("main_slider").load();
+	document.getElementById("main_slider").play();
+	document.getElementById("emoji").style.backgroundImage="url('"+songs[mood[val][index]-1][3]+"')";
+	songrun=true;
+}
+
+async function getTime() {
+                let value = await eel.getEmotion()();
+                if(value=="angry")
+                	moody(0);
+                else if(value=="happy")
+                	moody(1);
+                else if(value=="sad")
+                	moody(2);
+                else
+                	moody(3);
+            }
